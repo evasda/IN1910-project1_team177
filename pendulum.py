f717cab546290e248139d205d0b44488e4d03d06
evasda <evasda@math.uio.no>
2018-09-10 21:23:21 +0200

    removed unneccesary code

--- pendulum.py
+++ pendulum.py
@@ -1,3 +1,6 @@
+import os
+os.chdir("C:\\Users\\Eva\Desktop\\IN1910\\Prosjekt 1\\project1_team177\\")
+
 from scipy.integrate import solve_ivp
 import numpy as np
 import matplotlib.pyplot as plt
@@ -23,31 +26,39 @@ class Pendulum:
 		sol = solve_ivp(self.__call__, [0,T], y0, t_eval = list(np.linspace(0, T, T/dt)))
 		self._omega = sol.y[0]; self._theta = sol.y[1]
 		self._t = sol.t
-#		return [self._sol.t, self._sol.y]
 
 	@property
 	def t(self):
 		print("Setting t values")
 		return self._t
-	@t.setter
-	def t(self):
-		t = self._t
 
 	@property
 	def theta(self):
 		print("Getting theta values")
 		return self._theta
-	@theta.setter
-	def theta(self):
-		theta = self._theta
 
 	@property 
 	def omega(self):
 		print("Getting omega values")
 		return self._omega
-	@omega.setter
-	def omega(self):
-		omega = self._omega
+
+	@property 
+	def x(self):
+		print("x coordinates")
+		self._x = self.L * np.sin(self._theta)
+		return self._x
+
+	@property
+	def y(self):
+		print("y coordinates")
+		self._y = -self.L * np.cos(self._theta)
+		return self._y
+
+
+#	@property
+#	def p(self)
+#	return  
+
 
 
 
@@ -62,6 +73,8 @@ pen.solve(y0, T , dt, angles = "rad")
 print(pen.t)
 print(pen.theta)
 print(pen.omega)
+print(pen.y)
+print(pen.x)
 
 #print(t1, y1)
 
